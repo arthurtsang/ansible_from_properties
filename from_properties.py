e20c1dd0a2fea210357b01713f727adf93cb8cb3
Arthur Tsang <chiuwai.tsang@hp.com>
2016-03-17 20:20:18 -0700

    remove escape

--- from_properties.py
+++ from_properties.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import pyjq
-import re
 
 def from_properties(properties):
 	json = {}
@@ -9,7 +8,7 @@ def from_properties(properties):
 		if propertyLine.startswith('#'):
 			continue
 		keyValue = propertyLine.split('=',1)
-		json = pyjq.all("."+keyValue[0].strip()+'="'+re.escape(keyValue[1]).strip()+'"',json)[0]
+		json = pyjq.all("."+keyValue[0].strip()+'="'+keyValue[1].strip()+'"',json)[0]
 	return json
 
 class FilterModule(object):
